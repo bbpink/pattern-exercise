febaff1bce15dfad3c84a08ddd4bcdf168f15018
bbpink <ssbbpink@gmail.com>
2013-04-23 22:00:34 +0900

    add id number on FactoryMethod

--- FactoryMethod/idcard/IDCard.java
+++ FactoryMethod/idcard/IDCard.java
@@ -3,14 +3,16 @@ import framework.*;
 
 public class IDCard extends Product {
     private String owner;
+    private int id;
 
-    IDCard(String owner) {
-	System.out.println(owner + "のカードをつくりますー");
+    IDCard(String owner, int id) {
+	System.out.println(owner + "(" + id + ")のカードをつくりますー");
 	this.owner = owner;
+	this.id = id;
     }
 
     public void use() {
-	System.out.println(owner + "のカードを使ってる！！！");
+	System.out.println(owner + "(" + this.id + ")のカードを使ってる！！！");
     }
 
     public String getOwner() {
--- FactoryMethod/idcard/IDCardFactory.java
+++ FactoryMethod/idcard/IDCardFactory.java
@@ -6,7 +6,7 @@ public class IDCardFactory extends Factory {
     private List<String> owners = new ArrayList<String>();
 
     protected Product createProduct(String owner) {
-	return new IDCard(owner);
+	return new IDCard(owner, owners.size() + 1);
     }
 
     protected void registerProduct(Product product) {
