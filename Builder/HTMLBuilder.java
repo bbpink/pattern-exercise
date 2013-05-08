003d9aad78e5ee5b9c300623e32c430c9e968ef3
bbpink <ssbbpink@gmail.com>
2013-05-08 22:01:36 +0900

    raw Builder

---
+++ Builder/HTMLBuilder.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class HTMLBuilder extends Builder {
+    private String fn;
+    private PrintWriter pw;
+
+    public void makeTitle(String title) {
+	fn = title + ".html";
+
+	try {
+	    pw = new PrintWriter(new FileWriter(fn));
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	pw.println("<html><head><title>" + title + "</title></head></body>");
+	pw.println("<h1>" + title + "</h1>");
+    }
+
+    public void makeString(String s) {
+	pw.println("<p>" + s + "</p>");
+    }
+
+    public void makeItems(String[] items) {
+	pw.println("<ul>");
+	for (int i=0; i < items.length; i++) {
+	    pw.println("<li>" + items[i] + "</li>");
+	}
+	pw.println("</ul>");
+    }
+
+    public void close() {
+	pw.println("</body></html>");
+	pw.close();
+    }
+
+    public String getResult() {
+	return fn;
+    }
+}
---
+++ Builder/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+	if (args.length != 1) {
+	    usage();
+	    System.exit(0);
+	}
+
+	if (args[0].equals("plain")) {
+	    TextBuilder tb = new TextBuilder();
+	    Director d = new Director(tb);
+	    d.construct();
+	    String res = tb.getResult();
+	    System.out.println(res);
+	} else if (args[0].equals("html")) {
+	    HTMLBuilder hb = new HTMLBuilder();
+	    Director d = new Director(hb);
+	    d.construct();
+	    String res = hb.getResult();
+	    System.out.println(res + "が作成されました");
+	} else {
+	    usage();
+	    System.exit(0);
+	}
+    }
+
+    public static void usage() {
+	System.out.println("plain or html");
+    }
+}
